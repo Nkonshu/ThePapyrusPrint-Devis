d9ca1657bb517f0b9963e4e83dc33ccc81da414d
Guy Paulin <nkonshulion@yahoo.fr>
2020-08-05 11:05:05 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/the-papyrus-print-devis'));
+app.use(express.static('./dist/ThePapyrusPrint-Devis'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/the-papyrus-print-devis/'}),
-);
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/ThePapyrusPrint-Devis' }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
